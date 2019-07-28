080c4a3c582d3d102a50db98e053b0b8e6c2661a
tpadams <30413106+tpadams@users.noreply.github.com>
2019-07-28 19:10:56 +0100

    App 18-19

--- app-2018-19.R
+++ app-2018-19.R
@@ -114,6 +114,38 @@ names(gameweekpoints) <- c("Player","Week","Points","Cumulative") #create frame 
 gameweekpoints$Week <- as.double(levels(gameweekpoints$Week))[gameweekpoints$Week] #convert weeknumber from Factor to Numeric
 gameweekpoints <- gameweekpoints[!is.na(gameweekpoints$Week),]
 
+####Analysis 18-19####
+
+a<-gameweekpoints%>%
+  group_by(Week) %>%
+  mutate(rank=rank(-Cumulative,ties.method = "first"))
+
+x <- bind_rows(finaldf)
+x<-replace(x, is.na(x), 0)
+
+fourteenth<-lapply(finaldf,function(w){lapply(w[,c(11:48)],function(e){sort(e,TRUE)[25]})})
+
+fourteenth <- replace(unlist(fourteenth),is.na(unlist(fourteenth)),0)
+
+fourteenth[!is.na(match(fourteenth,4))]
+
+split(fourteenth,rep(1:5,each=38))
+
+nonscorers<-lapply(finaldf,function(w){lapply(w[,c(11:48)],function(e){sort(e,TRUE)[15:25]})})
+
+lapply(nonscorers,function(x){sum(unlist(x)>1,na.rm = T)})
+
+##
+q<-x %>%
+  group_by(`Picked by`) %>%
+  
+  mutate(gwrank=rank(row_number()))
+
+q[q$gwrank==14,]
+
+
+arrange(desc(`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`10`,`11`,`12`,`13`,`14`,`15`,`16`,`17`,`18`,`19`,`20`,`21`,`22`,`23`,`24`,`25`,`26`,
+             `27`,`28`,`29`,`30`,`31`,`32`,`33`,`34`,`35`,`36`,`37`,`38`,.by_group=T))
 
 #Define server logic required to summarize and view the selected data
 options(DT.options = list(paging=FALSE))
@@ -194,7 +226,7 @@ options(DT.options = list(paging=FALSE))
   
   #####TRANSFERS######
   
-  transfersDF <- data.frame(Player=c(),In=c(),Out=c(),Date=c())
+  transfersDF <- data.frame(Player=c("Hodge","Warnes","Tom","David"),In=c("Stanislas (BOU)","Neves (WOL)","Deulofeu (WAT)","Lindelof (MNU)"),Out=c("Pritchard (CAR)","Sanchez (TOT)","Mkhitaryan (ARS)","Ghezzal (LEI)"),Date=c("30/09/2018","7/12/2018","25/12/2018","28/12/2018"))
 
   output$transfers <- DT::renderDataTable(datatable(transfersDF))
   
