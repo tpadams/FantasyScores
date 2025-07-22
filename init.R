6dff37be97e086df2fa3940f7170be96149675e3
tpadams <30413106+tpadams@users.noreply.github.com>
2025-07-22 18:08:29 +0100

    Update init.R

--- init.R
+++ init.R
@@ -1,1 +1,1 @@
-helpers.installPackages("jsonlite","curl","data.table","dplyr","ggplot2","DT","shinydashboard","plotly","future","future.apply","curl","bs4dash","shinycssloaders")
+helpers.installPackages("jsonlite","curl","data.table","dplyr","ggplot2","DT","shinydashboard","plotly","future","future.apply","curl","bs4Dash","shinycssloaders")
